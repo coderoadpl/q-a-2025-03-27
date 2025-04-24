e03647aff72dd6c0b468cdf7a1df86be0a2f5203
Mateusz Choma <chomamateusz@gmail.com>
2025-04-24 20:28:28 +0200

    ✨ Include timeout for hiding unmatched cards

--- src/store.ts
+++ src/store.ts
@@ -42,91 +42,114 @@ export const createMemoryGameStore = ({
 }: {
   getTime: () => number
 }) => {
-  const memoryGameStateCreator: StateCreator<TMemoryGameState> = (set, getState) => ({
-    board: [],
-    startTime: null,
-    endTime: null,
-    startGame: (level: 'very-easy' | 'easy' | 'medium' | 'hard') => {
-      const numberOfCards = level === 'very-easy' ? 4 : level === 'easy' ? 8 : level === 'medium' ? 12 : 16
-      const cards = cardsDeck.slice(0, numberOfCards / 2)
-
-      const cardsToBeAddedOnBoard = [...cards, ...cards].sort(() => Math.random() - 0.5)
-      const cardsToBeAddedOnBoardWithIds = cardsToBeAddedOnBoard.map((card) => {
-        const cardRandomId = Math.random().toString(36).substring(2, 15)
-        return {
-          ...card,
-          id: cardRandomId,
-        }
-      })
-
-      const board = cardsToBeAddedOnBoardWithIds.map((card) => ({
-        ...card,
-        isFlipped: false,
-        isMatched: false,
-      }))
-
-      set({ board, startTime: getTime() })
-    },
-    flipCard: (id: string) => {
+  const memoryGameStateCreator: StateCreator<TMemoryGameState> = (set, getState) => {
+    let hideNotMatchedCardsTimeoutId: number | null = null
+    
+    const hideNotMatchedCards = () => {
       const board = getState().board
-      const card = board.find((card) => card.id === id)
-      if (!card) return
-      if (card.isFlipped) return
+      const allFlippedNotMatchedCards = board.filter((card) => card.isFlipped && !card.isMatched)
+      const allFlippedNotMatchedCardsIds = allFlippedNotMatchedCards.map((card) => card.id)
+      const newBoard = board.map((card) => {
+        if (allFlippedNotMatchedCardsIds.includes(card.id)) {
+          return { ...card, isFlipped: false }
+        }
+        return card
+      })
+      set({ board: newBoard })
+    }
 
-      const allFlippedCards = board.filter((card) => card.isFlipped)
-      const allFlippedCardsIds = allFlippedCards.map((card) => card.id)
-      const numberOfFlippedCards = allFlippedCards.length
+    return {
+      board: [],
+      startTime: null,
+      endTime: null,
+      startGame: (level: 'very-easy' | 'easy' | 'medium' | 'hard') => {
+        const numberOfCards = level === 'very-easy' ? 4 : level === 'easy' ? 8 : level === 'medium' ? 12 : 16
+        const cards = cardsDeck.slice(0, numberOfCards / 2)
 
-      if (numberOfFlippedCards === 2) {
-        const newBoard = board.map((card) => {
-          if (card.id === id) {
-            return { ...card, isFlipped: true }
+        const cardsToBeAddedOnBoard = [...cards, ...cards].sort(() => Math.random() - 0.5)
+        const cardsToBeAddedOnBoardWithIds = cardsToBeAddedOnBoard.map((card) => {
+          const cardRandomId = Math.random().toString(36).substring(2, 15)
+          return {
+            ...card,
+            id: cardRandomId,
           }
-          if (allFlippedCardsIds.includes(card.id)) {
-            return { ...card, isFlipped: false }
-          }
-          return card
         })
-        set({ board: newBoard })
-      } else {
-        set({
-          board: board.map((card) => {
+
+        const board = cardsToBeAddedOnBoardWithIds.map((card) => ({
+          ...card,
+          isFlipped: false,
+          isMatched: false,
+        }))
+
+        set({ board, startTime: getTime() })
+      },
+      flipCard: (id: string) => {
+        const board = getState().board
+        const card = board.find((card) => card.id === id)
+        if (!card) return
+        if (card.isFlipped) return
+
+        if (hideNotMatchedCardsTimeoutId) {
+          clearTimeout(hideNotMatchedCardsTimeoutId)
+        }
+
+        const allFlippedCards = board.filter((card) => card.isFlipped)
+        const allFlippedCardsIds = allFlippedCards.map((card) => card.id)
+        const numberOfFlippedCards = allFlippedCards.length
+
+        if (numberOfFlippedCards === 2) {
+          const newBoard = board.map((card) => {
             if (card.id === id) {
               return { ...card, isFlipped: true }
             }
+            if (allFlippedCardsIds.includes(card.id)) {
+              return { ...card, isFlipped: false }
+            }
             return card
           })
-        })
-      }
+          set({ board: newBoard })
+        } else {
+          set({
+            board: board.map((card) => {
+              if (card.id === id) {
+                return { ...card, isFlipped: true }
+              }
+              return card
+            })
+          })
+        }
 
-      const boardAfterFlipping = getState().board
+        hideNotMatchedCardsTimeoutId = setTimeout(hideNotMatchedCards, 2000)
 
-      const allFlippedCardsAfterFlipping = boardAfterFlipping.filter((card) => card.isFlipped)
-      const allFlippedCardsIdsAfterFlipping = allFlippedCardsAfterFlipping.map((card) => card.id)
-      if(allFlippedCardsAfterFlipping.length === 2) {
-        const allFlippedCardsValuesAfterFlipping = allFlippedCardsAfterFlipping.map((card) => card.value)
-        const areFlippedCardsMatchingAfterFlipping = allFlippedCardsValuesAfterFlipping.every((value, index, array) => value === array[0])
-      
-        const newBoard = boardAfterFlipping.map((card) => {
-          if (allFlippedCardsIdsAfterFlipping.includes(card.id)) {
-            return { ...card, isMatched: areFlippedCardsMatchingAfterFlipping }
-          }
-          return card
-        })
-        set({ board: newBoard })
-      }
-      
-      const finalBoard = getState().board
+        const boardAfterFlipping = getState().board
 
-      const allMatchedCards = finalBoard.filter((card) => card.isMatched)
-      if (allMatchedCards.length === finalBoard.length) {
-        set({ endTime: getTime() })
-      }
-    },
-    restartGame: () => {
-      set({ board: [], startTime: null, endTime: null })
-    },
-  })
+        const allFlippedCardsAfterFlipping = boardAfterFlipping.filter((card) => card.isFlipped)
+        const allFlippedCardsIdsAfterFlipping = allFlippedCardsAfterFlipping.map((card) => card.id)
+        if (allFlippedCardsAfterFlipping.length === 2) {
+          const allFlippedCardsValuesAfterFlipping = allFlippedCardsAfterFlipping.map((card) => card.value)
+          const areFlippedCardsMatchingAfterFlipping = allFlippedCardsValuesAfterFlipping.every((value, index, array) => value === array[0])
+
+          const newBoard = boardAfterFlipping.map((card) => {
+            if (allFlippedCardsIdsAfterFlipping.includes(card.id)) {
+              return { ...card, isMatched: areFlippedCardsMatchingAfterFlipping }
+            }
+            return card
+          })
+          set({ board: newBoard })
+        }
+
+        const finalBoard = getState().board
+
+        const allMatchedCards = finalBoard.filter((card) => card.isMatched)
+        if (allMatchedCards.length === finalBoard.length) {
+          set({ endTime: getTime() })
+        }
+      },
+      restartGame: () => {
+        set({ board: [], startTime: null, endTime: null })
+      },
+    }
+  }
 
   const memoryGameStore = createStore<TMemoryGameState>()(
     persist(
