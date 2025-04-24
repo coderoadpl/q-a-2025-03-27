bc91f322aaa400173193772e213348d27cf3a4cf
Mateusz Choma <chomamateusz@gmail.com>
2025-04-24 20:51:25 +0200

    💄 Enhance `Card` component with 3D flip animation

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,4 +1,4 @@
-import { Paper } from "@mui/material";
+import { Paper, Box } from "@mui/material";
 
 export interface CardProps {
   id: string;
@@ -10,34 +10,58 @@ export interface CardProps {
 
 export const Card = ({ id, value, isFlipped, isMatched, onClick }: CardProps) => {
   return (
-    <Paper
-      key={id}
-      onClick={onClick}
-      elevation={1}
-      sx={{
-        aspectRatio: '1/1',
+    <Box
+      style={{
+        position: 'relative',
         width: '100px',
         height: '100px',
-        display: 'flex', 
-        alignItems: 'center', 
-        justifyContent: 'center',
-        fontSize: '1.875rem', 
-        borderRadius: 1,
-        cursor: onClick ? 'pointer' : 'default',
-        bgcolor: isMatched 
-          ? 'success.light' 
-          : isFlipped 
-            ? 'primary.light' 
-            : 'grey.300',
-        transition: 'background-color 0.2s',
-        '&:hover': {
-          bgcolor: onClick 
-            ? 'grey.400' 
-            : undefined
-        }
+        transformStyle: 'preserve-3d',
+        transition: 'transform 0.6s',
+        transform: isFlipped || isMatched ? 'rotateY(180deg)' : 'rotateY(0deg)',
       }}
     >
-      {(isFlipped || isMatched) ? value : '?'}
-    </Paper>
+      <Paper
+        key={`${id}-front`}
+        onClick={onClick}
+        elevation={1}
+        sx={{
+          position: 'absolute',
+          width: '100%',
+          height: '100%',
+          backfaceVisibility: 'hidden',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          fontSize: '1.875rem',
+          borderRadius: 1,
+          cursor: onClick ? 'pointer' : 'default',
+          bgcolor: 'grey.300',
+          '&:hover': {
+            bgcolor: onClick ? 'grey.400' : undefined
+          }
+        }}
+      >
+        ?
+      </Paper>
+      <Paper
+        key={`${id}-back`}
+        elevation={1}
+        sx={{
+          position: 'absolute',
+          width: '100%',
+          height: '100%',
+          backfaceVisibility: 'hidden',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          fontSize: '1.875rem',
+          borderRadius: 1,
+          bgcolor: isMatched ? 'success.light' : 'primary.light',
+          transform: 'rotateY(180deg)',
+        }}
+      >
+        {value}
+      </Paper>
+    </Box>
   );
 }; 
